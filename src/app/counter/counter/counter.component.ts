56bf8bc31c945008b53c0a8728cce6190efea214
Hriday <hridayjadhav789@gmail.com>
2023-10-11 10:46:56 +0530

    Here we have passed the data via input and output

---
+++ src/app/counter/counter/counter.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  templateUrl: './counter.component.html',
+  styleUrls: ['./counter.component.scss'],
+})
+export class CounterComponent {
+  counter: number = 0;
+
+  constructor() {}
+
+  onIncrement() {
+    this.counter++;
+  }
+
+  onDecrement() {
+    this.counter--;
+  }
+
+  onReset() {
+    this.counter = 0;
+  }
+}
