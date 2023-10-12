d23c01f7ac68e4d54febe9741045900d7cbea625
Hriday <hridayjadhav789@gmail.com>
2023-10-12 12:39:42 +0530

    Using Async Pipe for selecting observable state data

--- src/app/counter/counter-output/counter-output.component.ts
+++ src/app/counter/counter-output/counter-output.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit} from '@angular/core';
 import { Store } from '@ngrx/store';
+import { CounterState } from '../state/counter.state';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-counter-output',
@@ -7,12 +9,12 @@ import { Store } from '@ngrx/store';
   styleUrls: ['./counter-output.component.scss'],
 })
 export class CounterOutputComponent implements OnInit{
-  counter : any;
-  constructor(private store : Store<{counter: {counter: number}}>){}
+
+  counter$!: Observable<{ counter: number }>; 
+
+  constructor(private store : Store<{counter: CounterState}>){}
 
   ngOnInit(): void {
-    this.store.select('counter').subscribe(data => {
-      this.counter = data.counter
-    })
+    this.counter$ = this.store.select('counter');
   }
 }
