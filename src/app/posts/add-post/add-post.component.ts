db9e24e7538d6b7e8705866670732f50685a1925
Hriday <hridayjadhav789@gmail.com>
2023-10-17 11:36:20 +0530

    Adding Posts into the ngrx state submitted by the Form

--- src/app/posts/add-post/add-post.component.ts
+++ src/app/posts/add-post/add-post.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { Post } from 'src/app/models/posts.model';
+import { AppState } from 'src/app/store/app.state';
+import { addPost } from '../state/posts.actions';
 
 @Component({
   selector: 'app-add-post',
@@ -9,7 +13,7 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 export class AddPostComponent implements OnInit {
   postForm!: FormGroup;
 
-  constructor() {}
+  constructor(private store: Store<AppState>) {}
 
   ngOnInit(): void {
     this.postForm = new FormGroup({
@@ -22,11 +26,16 @@ export class AddPostComponent implements OnInit {
     if(!this.postForm.valid){
       return;
     }
-
-    console.log(this.postForm.value);
+    const post : Post = {
+      title : this.postForm.value.title,
+      description : this.postForm.value.description,
+    };   
     
-    
+    this.store.dispatch(addPost({ post }))
+    // after calling addpost, we have to send the post data, that's why I use this. 
   }
+
+
 
   showTitleErrors() {
   const titleForm = this.postForm.get('title');
--- src/app/posts/state/posts.reducer.ts
+++ src/app/posts/state/posts.reducer.ts
@@ -1,7 +1,18 @@
-import { createReducer } from "@ngrx/store";
+import { createReducer, on } from "@ngrx/store";
 import { initialState } from "./posts.state";
+import { addPost } from "./posts.actions";
 
-const _postsReducer = createReducer(initialState)
+
+//we need to update addPost, that's why we use addPost, and it will take the state as an argument,
+//so in the method  we have to return the state and here we have to upgrade the new post.
+const _postsReducer = createReducer(initialState, on(addPost, (state, action) =>{
+    let post = { ...action.post };
+    post.id = state.posts.length +1;   //this is bcz we have to take a new id to add in the table.
+    return {
+        ...state,
+        posts : [...state.posts, post],
+    } 
+}))
 
 export function postsReducer(state: any, action: any){
     return _postsReducer(state, action);
